a6a3cf1f45f0038c5396763dbcef677c03ada07d
mjaumi <mjaumi2864@gmail.com>
2022-04-04 02:34:46 +0600

    404 component and route done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
         <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </main>
   );
