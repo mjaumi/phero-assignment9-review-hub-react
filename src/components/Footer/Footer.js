eabc41cfc7e18e5adc4b3555ba7c57ead55dfa36
mjaumi <mjaumi2864@gmail.com>
2022-04-04 19:29:51 +0600

    review component responsive done & footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className='py-5'>
+            <p className='text-review-hub-base-green font-light text-lg'>©Milhan Joardar Aumi, 2022.</p>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -8,8 +8,8 @@ const Reviews = () => {
 
     return (
         <section className='min-h-screen py-28'>
-            <h2 className='text-4xl text-review-hub-base-green font-light'>What Our Customers Say!</h2>
-            <div className='w-4/5 mx-auto grid grid-cols-3 gap-10 mt-10'>
+            <h2 className='text-2xl md:text-4xl text-review-hub-base-green font-light'>What Our Customers Say!</h2>
+            <div className='w-[90%] lg:w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-10'>
                 {
                     reviews.map(review => <Review
                         key={review.id}
